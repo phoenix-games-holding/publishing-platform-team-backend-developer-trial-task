fd04d89da334f4c676d66316e83a3ca4b43737c5
Artem Volk <artem.volk@phoenixgames.com>
2023-05-09 12:45:10 +0200

    Extracting class member

--- src/test/java/com/spotlight/platform/userprofile/api/core/json/JsonMapperTest.java
+++ src/test/java/com/spotlight/platform/userprofile/api/core/json/JsonMapperTest.java
@@ -1,5 +1,6 @@
 package com.spotlight.platform.userprofile.api.core.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spotlight.platform.helpers.FixtureHelpers;
 
 import org.junit.jupiter.api.Test;
@@ -7,19 +8,20 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class JsonMapperTest {
+    private static final ObjectMapper JSON_MAPPER = JsonMapper.getInstance();
 
     private static final String FIXTURE_PATH = "/fixtures/core/json/testEntityWithInstantsIsoString.json";
 
     @Test
     void serializeInstants_CorrectlySerialized() throws Exception {
-        var serializedEntity = JsonMapper.getInstance().writeValueAsString(new TestEntityWithInstants());
+        var serializedEntity = JSON_MAPPER.writeValueAsString(new TestEntityWithInstants());
 
         assertThat(serializedEntity).isEqualToIgnoringWhitespace(FixtureHelpers.fixture(FIXTURE_PATH));
     }
 
     @Test
     void deserializeInstants_CorrectlyDeserialized() throws Exception {
-        var deserializedEntity = JsonMapper.getInstance().readValue(FixtureHelpers.fixture(FIXTURE_PATH), TestEntityWithInstants.class);
+        var deserializedEntity = JSON_MAPPER.readValue(FixtureHelpers.fixture(FIXTURE_PATH), TestEntityWithInstants.class);
 
         assertThat(deserializedEntity).usingRecursiveComparison().isEqualTo(new TestEntityWithInstants());
     }
